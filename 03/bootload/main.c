f70f33306829779a60233789790ae10922553193
imadedede <imadedede@gmail.com>
2020-02-12 22:58:01 +0900

    初期化処理を追加 静的領域をROMからRAMへコピー

--- 03/bootload/main.c
+++ 03/bootload/main.c
@@ -2,13 +2,30 @@
 #include "serial.h"
 #include "lib.h"
 
+static int init(void) {
+    // 以下はリンカ・スクリプトで定義してあるシンボル
+    // リンカ・スクリプトで定義されたシンボルを参照可能にする
+    extern int erodata;
+    extern int data_start;
+    extern int edata;
+    extern int bss_start;
+    extern int ebss;
+
+    // 静的領域(データ領域とBSS領域)を初期化する
+    // この処理以降でないとグローバル変数が初期化されていないので注意
+    memcpy(&data_start, &erodata, (long)&edata - (long)&data_start);
+    memset(&bss_start, 0, (long)&ebss - (long)&bss_start);
+
+    // シリアルの初期化
+    serial_init(SERIAL_DEFAULT_DEVICE); // シリアルデバイスの初期化
+
+    return 0;
+}
+
 int main(void) {
-    serial_init(SERIAL_DEFAULT_DEVICE);
 
+    init();     // 初期化関数の呼び出し
     puts("Hello World!\n");
-
-    putxval(0x10, 0);   puts("\n");
-    putxval(0xffff, 0); puts("\n");
 
     while (1)
         ;
