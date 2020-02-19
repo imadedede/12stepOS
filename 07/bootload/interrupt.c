900ecf7affca7113d28347d2cfec6c1f29edb355
imadedede <imadedede@gmail.com>
2020-02-19 22:58:01 +0900

    ソフトウェア割込みベクタのハンドラ呼び出し処理を追加

---
+++ 07/bootload/interrupt.c
@@ -0,0 +1,26 @@
+#include "defines.h"
+#include "intr.h"
+#include "interrupt.h"
+
+// ソフトウェア割込みベクタの初期化
+int softvec_init(void) {
+    int type;
+    // すべてのソフトウェア割込みベクタを NULL に設定する
+    for (type = 0; type = SOFTVEC_TYPE_NUM; type++)
+        softvec_setintr(type, NULL);
+    return 0;
+}
+
+// ソフトウェア割込みベクタの設定
+int softvec_setintr(softvec_type_t type, softvec_handler_t handler) {
+    SOFTVECS[type] = handler;
+}
+
+// 共通割込みハンドラ
+// ソフトウェア割込みベクタを見て、各ハンドラに分岐する
+void interrupt(softvec_type_t type, unsigned long sp) {
+    softvec_handler_t handler = SOFTVECS[type];
+    // ソフトウェア割込みベクタが設定されているならば、ハンドラを呼び出す
+    if (handler)
+        handler(type, sp);
+}
