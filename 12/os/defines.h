4b5a33b184e92c855a5f122f4338689caf42cd3d
imadedede <imadedede@gmail.com>
2020-02-28 13:59:09 +0900

    12章 外部割込みを実装 準備

---
+++ 12/os/defines.h
@@ -0,0 +1,23 @@
+#ifndef _DEFINES_H_INCLUDED_
+#define _DEFINES_H_INCLUDED_
+
+#define NULL ((void *)0)
+#define SERIAL_DEFAULT_DEVICE 1
+
+typedef unsigned char  uint8;
+typedef unsigned short uint16;
+typedef unsigned long  uint32;
+
+// OS で利用される型を定義
+typedef uint32 kz_thread_id_t;                      // スレッド ID
+typedef int (*kz_func_t)(int argc, char *argv[]);   // スレッドのメイン関数の型
+typedef void (*kz_handler_t)(void);                 // 割込みハンドラの型
+
+// メッセージ ID の定義
+typedef enum {
+    MSGBOX_ID_MSGBOX1 = 0,
+    MSGBOX_ID_MSGBOX2,
+    MSGBOX_ID_NUM
+} kz_msgbox_id_t;
+
+#endif
